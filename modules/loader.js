72d02321c3bb6437ed9b68550134532794afc8f4
qt <splitxplayzofficial@outlook.com>
2021-12-05 03:01:05 +0100

    Command handler

---
+++ modules/loader.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const util = require('util')
+const promisify = util.promisify
+const readdir = promisify(fs.readdir)
+
+module.exports = (client) => {
+  let i = 1
+
+  readdir(__dirname + '/../commands/', (err, files) => {
+    if (err) return console.log(err)
+    files.forEach((file) => {
+      if (!file.endsWith('.js')) return
+      const props = require(`../commands/${file}`)
+      const commandName = file.split('.')[0]
+
+      console.log(`[Bot.Commands] Loaded ${commandName} (${i}) command.`)
+
+      client.commands.set(commandName, props)
+      props.conf.aliases.forEach((al) => {
+        client.aliases.set(al, client.commands.get(commandName))
+      })
+
+      i++
+    })
+  })
+}
