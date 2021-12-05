721979371dd167727cbdabe025d6afd885605ed7
qt <splitxplayzofficial@outlook.com>
2021-12-05 03:04:59 +0100

    Create event.js

---
+++ modules/handlers/event.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const util = require('util')
+const promisify = util.promisify
+const readdir = promisify(fs.readdir)
+
+module.exports = (client) => {
+   let i = 1
+	
+  readdir(__dirname + '/../events/', (err, files) => {
+    files.forEach(file => {
+      if (!file.endsWith('.js')) return
+      const event = require(`../events/${file}`)
+      const eventName = file.split('.')[0]
+
+      client.on(eventName, event.bind(null, client))
+
+      delete require.cache[require.resolve(`../events/${file}`)]
+
+      console.log(`[Events]: Loaded ${eventName} (${i}) event.`)
+     })
+
+      i++
+    })
+  }
