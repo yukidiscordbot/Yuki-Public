97a082a113bbc9246a6bed817f2444e9adce646f
qt <splitxplayzofficial@outlook.com>
2021-12-08 13:30:50 +0100

    Fully updated code

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-if (process.version < 13) throw new Error('You must upgrade your node version. You need node v13 atleast to run this bot.')
+const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
+ if (NODE_MAJOR_VERSION < 16) {
+  throw new Error(`Your node version ${process.versions.node.split('.')[0]} is outdated, please upgrade it to node V16.6!`);
+  process.exit(1);
+}
 require('dotenv').config()
 
 const Discord = require('discord.js')
 const Enmap = require('enmap')
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ disableEveryone: true, disabledEvents: ["TYPING_START"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+//Enmap Database Generation
+client.points = new Enmap({ name: 'points' })
+client.inventory = new Enmap({ name: 'inventory' })
+client.money = new Enmap({ name: 'money' })
+client.bank = new Enmap({ name: 'bank' })
+client.cooldown = new Enmap({ name: 'cooldown' })
+client.rep = new Enmap({ name: 'rep' })
+client.settings = new Enmap({ name: 'settings' })
+client.warns = new Enmap({ name: 'warns' })
+client.life = new Enmap({ name: 'life' })
+client.tags = new Enmap({ name: 'tags' })
+client.uses = new Enmap({ name: 'uses' })
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
 client.music = {}
 client.levelCache = {}
 
-client.config = require('./config.js')
-require('./modules/handler/command')(client)
-require('./modules/handler/event')(client)
+client.config = require('./config')
+require('./modules/handlers/command')(client)
+require('./modules/handlers/event')(client)
 require('./modules/music')(client)
 
 //logical todo use 0,1 instead false, true
@@ -25,8 +44,8 @@ client.ytdl = ytdl;
 }
 
 for (let i = 0; i < client.config.permLevels.length; i++) {
-  const clevel = client.config.permLevels[i]
-  client.levelCache[clevel.name] = clevel.level
+    let currentlevel = client.config.permLevels[i]
+    client.levelCache[currentlevel.name] = currentlevel.level
 }
 
-client.login('token').catch(console.error);
+client.login('REPLACEMEHERE').catch(console.error);
